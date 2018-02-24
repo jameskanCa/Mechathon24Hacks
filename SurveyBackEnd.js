62f89ca10046b9a2f2e5d81aa52fa9e11a18817d
James Kan <32029905+jameskanCa@users.noreply.github.com>
2018-02-24 00:32:29 -0800

    Add files via upload
    Update 5 - james

--- SurveyBackEnd.js
+++ SurveyBackEnd.js
@@ -1,9 +1,17 @@
-function update(){
-    _e('btn').addEventListener('click', function() {
-    _e('demo').innerHTML = 'Hey There';
-  }, false);
+var text = document.getElementById("question");
+var responseButton1 = document.getElementById('but').addEventListener("click",swap());
+var progressCounter = document.getElementById("num").value;
 
-function _e(id) {
-   return document.getElementById(id);
- }
+function SurveyBackEnd(){
+  this.progressValue = 0;
+}
+
+function swap(){
+
+var progressValue = parseInt(progressCounter);
+
+var questionBank  = ["Who are you seeking help for?", "Have you been to rehab before?"]
+var responseOption = [["Myself","Someone I know"] , ["Yes", "No"]];
+
+
 }
